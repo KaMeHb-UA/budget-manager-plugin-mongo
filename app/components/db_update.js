a969f6726a180f3fd94852e3c936deeba8d1aa65
KaMeHb-UA <marlock@bassteam.ml>
2021-08-23 20:14:34 +0300

    fix db_update

--- app/components/db_update.js
+++ app/components/db_update.js
@@ -2,10 +2,6 @@ import { ObjectId } from 'mongodb';
 import db from '../helpers/mongo.js';
 
 export default async ({ table, id, data }) => {
-    await db.collection(table).updateOne({
-        _id: new ObjectId(id),
-    }, {
-        $literal: data,
-    });
+    await db.collection(table).replaceOne({ _id: new ObjectId(id) }, data);
     return null;
 }
